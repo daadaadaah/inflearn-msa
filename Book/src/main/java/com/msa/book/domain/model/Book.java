6893675cb6533f2c74fd1d4ed589d3131dcc20d1
daadaadaah <daadaadaah@gmail.com>
2023-11-16 15:26:50 +0900

    [EDA 기반 Microservice 구현-섹션3] 도메인 모델 개발(도메인 헥사곤) - 비즈니스 로직 구현

--- Book/src/main/java/com/msa/book/domain/model/Book.java
+++ Book/src/main/java/com/msa/book/domain/model/Book.java
@@ -4,6 +4,8 @@ import com.msa.book.domain.model.vo.BookDesc;
 import com.msa.book.domain.model.vo.BookStatus;
 import com.msa.book.domain.model.vo.Classfication;
 import com.msa.book.domain.model.vo.Location;
+import com.msa.book.domain.model.vo.Source;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +20,34 @@ public class Book {
     private Classfication classfication;
     private BookStatus bookStatus;
     private Location location;
+
+    public static Book enterBook(String title, String author, String isbn, String description, LocalDate publicationDate, Source source, Classfication classfication, Location location) {
+        BookDesc bookDesc = BookDesc.createBookDesc(author, isbn, description, publicationDate, source);
+
+        Book book = new Book();
+        book.setTitle(title);
+        book.setDesc(bookDesc);
+        book.setClassfication(classfication);
+        book.setLocation(location);
+        book.setBookStatus(BookStatus.ENTERED);
+        return book;
+    }
+
+    public static Book sample(){
+        return enterBook("엔터프라이즈 아키텍처 패턴","마틴파울러","21321321","엔터프라이즈 패턴 에 관한 좋은 서적", LocalDate.now(),
+            Source.SUPPLY,
+            Classfication.COMPUTER,
+            Location.JEONGJA);
+    }
+
+    public Book makeAvailable() {
+        this.setBookStatus(BookStatus.AVAILABLE);
+        return this;
+    }
+
+    public Book makeUnavailable() {
+        this.setBookStatus(BookStatus.AVAILABLE);
+        return this;
+    }
 }
 
--- Book/src/main/java/com/msa/book/domain/model/vo/BookDesc.java
+++ Book/src/main/java/com/msa/book/domain/model/vo/BookDesc.java
@@ -15,4 +15,13 @@ public class BookDesc {
     private String isbn;
     private LocalDate publicationDate;
     private Source source;
+
+    public static BookDesc createBookDesc(String author, String isbn, String description, LocalDate publicationDate, Source source) {
+        return new BookDesc(description, author, isbn, publicationDate, source);
+    }
+
+    public static BookDesc sample() {
+        return createBookDesc("마틴파울러", "123456789",
+            "엔터프라이즈 아키텍처 패턴을 설명", LocalDate.now(), Source.SUPPLY);
+    }
 }
